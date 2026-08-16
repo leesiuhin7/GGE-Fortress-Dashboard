eca52abf84974c36d65c9075a444fd708c3c0145
leesiuhin7 <leesiuhin7@gmail.com>
2026-08-17 00:14:51 +0100

    ci: add github workflow for deploying to GitHub Pages
    - Allow base path to be configured during build time
    
    - Add deploy.yml for deployment CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [react()],
+  };
+});
