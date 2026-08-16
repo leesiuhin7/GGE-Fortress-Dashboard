99242fab6b1bc1dc3679f2ca84b0eba20c056596
leesiuhin7 <leesiuhin7@gmail.com>
2026-08-17 00:08:37 +0100

    feat: add top-level orchestration layer
    - Complete App.tsx to orchestrate all components
    
    - Modify index.css to make the entire page used

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useRef, useState } from "react";
+import type { Location } from "./locations";
+import LocationDisplay, { Fetcher } from "./locations";
+
 function App() {
-  return <></>;
+  const [locations, setLocations] = useState<Location[]>([]);
+
+  const fetcher = useRef(new Fetcher((locations) => setLocations(locations)));
+  useEffect(() => fetcher.current.start(), []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "stretch",
+        padding: 50,
+        justifyContent: "center",
+      }}
+    >
+      <LocationDisplay locations={locations} />
+    </div>
+  );
 }
 
 export default App;
